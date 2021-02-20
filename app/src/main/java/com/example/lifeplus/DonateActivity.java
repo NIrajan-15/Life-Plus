0806dd2cb9c716cd7f275ee4e0ba7ff18ceaf196
Nirajan Sangraula <neerazansangraula14@gmail.com>
2021-02-19 19:02:00 -0600

    Login Workflow

---
+++ app/src/main/java/com/example/lifeplus/DonateActivity.java
@@ -0,0 +1,17 @@
+package com.example.lifeplus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DonateActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/lifeplus/ReceiveActivity.java
@@ -0,0 +1,15 @@
+package com.example.lifeplus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceiveActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+
+
+    }
+}
